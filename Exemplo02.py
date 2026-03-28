e260c01b0c7cce9ae06e86c78150ca737b58f555
Millena S. Zovadelli <millenaszovadelli@gmail.com>
2026-03-28 20:47:22 -0300

    Aula 02 - Variáveis, Operações, Entrada e Saida de Dados

---
+++ Exemplo02.py
@@ -0,0 +1,11 @@
+# Entrada de Dados
+'''
+Esse trecho de codigo mostra como ler dados informados pelo usuario com a utilização do metodo input(). Este metodo retorna a string informada pelo usuario.
+'''
+
+# obtém o nome e a idade do usuario.
+
+nome = input("informe o seu nome: ")
+idade = int(input("informe a sua idade: "))
+
+print(f"Olá, {nome}, Uau! Você tem {idade} anos também. você gosta de Python?")
---
+++ Exemplo03.py
@@ -0,0 +1,15 @@
+# Operações Aritméticos
+
+a = 10; b = 20; c = 30
+
+d = a + b # Resultado: 30
+print(d)
+e = c / a # Resultado: 3.0 
+print(e)
+f = a - b # Resultado: -10
+print(f)
+g = b * c # Resultado: 600
+print(g)
+h = 3
+i = b / h # Resultado: 6.66
+print(i)
